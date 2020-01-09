9bb15745c17d2c92689e08065294715673ed7f4b
Ionut Cicio <cicioionut328@gmail.com>
2020-01-09 17:13:37 +0100

    added other pages

---
+++ script.js
@@ -0,0 +1,11 @@
+function scrollAboutme() {
+    scrollTo(0, window.innerHeight);
+}
+
+function scrollProjects() {
+    scrollTo(0, window.innerHeight * 2)
+}
+
+function scrollContacts() {
+    scrollTo(0, window.innerHeight * 3)
+}
