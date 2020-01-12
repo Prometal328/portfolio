16dce39eb1994d60c0ca4e1a8fcf89bf8e5388ea
Ionut Cicio <cicioionut328@gmail.com>
2020-01-12 19:33:48 +0100

    corrected background and formatting

--- canvas/sketch.js
+++ canvas/sketch.js
@@ -1,59 +1,109 @@
 window.addEventListener('resize', () => location.reload());
 
-const border = 10;
+const border = -10;
 
-class Dot {
+class Point {
     constructor() {
-        this.x = random(border, width - border);
+        this.x = random(border, (width - border) * 0.5);
         this.y = random(border, height - border);
-    }
 
-    connect(alpha, beta) {
-        let alphaDistance = Math.sqrt(Math.pow(alpha.x - this.x, 2) + Math.pow(alpha.y - this.y, 2));
-        let betaDistance = Math.sqrt(Math.pow(beta.x - this.x, 2) + Math.pow(beta.y - this.y, 2));
+        this.travel = 0;
+        this.maxDistance = random(4, 10);
 
-        if (alphaDistance < 300 && betaDistance < 300) {
-            noStroke();
-            fill(255, 255, 255, 5);
-            triangle(this.x, this.y, alpha.x, alpha.y, beta.x, beta.y);
-        }
+        this.xAcceleration = random(-0.5, 0.5);
+        this.yAcceleration = random(-0.5, 0.5);
     }
 
     move() {
-        if (this.x > border && this.x < width - border) {
-            this.x += random(-1, 1);
+        if (this.travel < this.maxDistance) {
+            this.x += this.xAcceleration;
+            this.y += this.yAcceleration;
         } else {
-            this.x = random(border, width - border);
+            this.resetAcceleration();
         }
 
-        if (this.y > border && this.y < height - border) {
-            this.y += random(-1, 1);
+        if (this.x < border) {
+            this.x += 2;
         } else {
-            this.y = random(border, height - border);
+            this.travel = 0;
+            this.xAcceleration *= -1;
+        }
+
+        if (this.x > width - border) {
+            this.x -= 2;
+        } else {
+            this.travel = 0;
+            this.xAcceleration *= -1;
+        }
+
+        if (this.y < border) {
+            this.y += 2;
+        } else {
+            this.travel = 0;
+            this.yAcceleration *= -1;
+        }
+
+        if (this.y > height - border) {
+            this.y -= 2;
+        } else {
+            this.travel = 0;
+            this.yAcceleration *= -1;
         }
     }
-};
 
-let dots = [] 
+    resetAcceleration() {
+        travel = 0;
+
+        this.xAcceleration *= random(-0.1, 5);
+        this.yAcceleration *= random(-0.1, 5);
+    }
+}
+
+class Triangle {
+    constructor() {
+        let increment = random(0, (width - border * 2) * 0.5);
+
+        this.alpha = new Point();
+        this.alpha.x += increment;
+        this.beta = new Point();
+        this.beta.x += increment;
+        this.gamma = new Point();
+        this.gamma.x += increment;
+
+        this.color = color(251, 126, 105, random(10, 20));
+        if (random(-1, 1) > 0) {
+            this.color = color(31, 44, 99, random(10, 20));
+        }
+    }
+
+    display() {
+        fill(this.color);
+        noStroke();
+        triangle(this.alpha.x, this.alpha.y, this.beta.x, this.beta.y, this.gamma.x, this.gamma.y);
+    }
+
+    move() {
+        this.alpha.move();
+        this.beta.move();
+        this.gamma.move();
+    }
+}
+
+let triangles = []
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    let dotsCount = random(30, 40);
+    createCanvas(windowWidth, windowHeight + 1);
 
-    for (_ = 0; _ < dotsCount; _++) {
-        dots.push(new Dot());
+    for (_ = 0; _ < 10; _++) {
+        triangles.push(new Triangle());
     }
 }
 
 function draw() {
     background(0);
 
-    dots.forEach(alpha => {
-        dots.forEach(beta => {
-            dots.forEach(gamma => {
-                alpha.connect(beta, gamma);
-                alpha.move();
-            });
-        });
+    triangles.forEach(triangle => {
+        triangle.display();
+        triangle.move();
     });
 }
