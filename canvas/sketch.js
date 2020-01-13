f2932e5115cec5dea5dd92b586f8db0f35b0e089
Ionut Cicio <cicioionut328@gmail.com>
2020-01-13 18:01:24 +0100

    added final background removed aboutme background

--- canvas/sketch.js
+++ canvas/sketch.js
@@ -1,109 +1,31 @@
 window.addEventListener('resize', () => location.reload());
 
-const border = -10;
+const border = 100;
 
-class Point {
+class Particle {
     constructor() {
-        this.x = random(border, (width - border) * 0.5);
+        this.x = random(border, width - border);
         this.y = random(border, height - border);
-
-        this.travel = 0;
-        this.maxDistance = random(4, 10);
-
-        this.xAcceleration = random(-0.5, 0.5);
-        this.yAcceleration = random(-0.5, 0.5);
-    }
-
-    move() {
-        if (this.travel < this.maxDistance) {
-            this.x += this.xAcceleration;
-            this.y += this.yAcceleration;
-        } else {
-            this.resetAcceleration();
-        }
-
-        if (this.x < border) {
-            this.x += 2;
-        } else {
-            this.travel = 0;
-            this.xAcceleration *= -1;
-        }
-
-        if (this.x > width - border) {
-            this.x -= 2;
-        } else {
-            this.travel = 0;
-            this.xAcceleration *= -1;
-        }
-
-        if (this.y < border) {
-            this.y += 2;
-        } else {
-            this.travel = 0;
-            this.yAcceleration *= -1;
-        }
-
-        if (this.y > height - border) {
-            this.y -= 2;
-        } else {
-            this.travel = 0;
-            this.yAcceleration *= -1;
-        }
-    }
-
-    resetAcceleration() {
-        travel = 0;
-
-        this.xAcceleration *= random(-0.1, 5);
-        this.yAcceleration *= random(-0.1, 5);
-    }
-}
-
-class Triangle {
-    constructor() {
-        let increment = random(0, (width - border * 2) * 0.5);
-
-        this.alpha = new Point();
-        this.alpha.x += increment;
-        this.beta = new Point();
-        this.beta.x += increment;
-        this.gamma = new Point();
-        this.gamma.x += increment;
-
-        this.color = color(251, 126, 105, random(30, 40));
-        if (random(-1, 1) > 0) {
-            this.color = color(31, 44, 99, random(30, 40));
-        }
+        this.radius = random(5, 10);
     }
 
     display() {
-        fill(this.color);
-        noStroke();
-        triangle(this.alpha.x, this.alpha.y, this.beta.x, this.beta.y, this.gamma.x, this.gamma.y);
     }
 
     move() {
-        this.alpha.move();
-        this.beta.move();
-        this.gamma.move();
     }
 }
 
-let triangles = []
+let particles = []
 
 function setup() {
     createCanvas(windowWidth, windowHeight + 1);
 
-    for (_ = 0; _ < 15; _++) {
-        triangles.push(new Triangle());
+    for (_ = 0; _ < 50; _++) {
+        particles.push(new Particle());
     }
 }
 
 function draw() {
     background(0);
-
-    triangles.forEach(triangle => {
-        triangle.display();
-        triangle.move();
-    });
 }
